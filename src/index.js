8dca23f819def6a02ead017b6ab8e86d4d093e53
조영재 <wo_123@naver.com>
2021-06-25 16:15:37 +0900

    mmmm

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Root from './test/Root';
+import App from './test/App';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <Root />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
