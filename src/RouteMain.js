0e2bbdfc4fdee1945777bb99feb859c3e0faefaa
yjs24 <yunjs24@gmail.com>
2021-06-29 16:26:35 +0900

    junsu 작업물 추가

--- src/RouteMain.js
+++ src/RouteMain.js
@@ -1,19 +1,22 @@
-import { BrowserRouter, Route } from "react-router-dom";
+ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import RouteHyunjin from "./hyunjin/RouteHyunjin";
 import Cart from "./purchase/Cart";
 import Menu from "./Menu";
 import ProductListApp from './youngjae/ProductListApp';
 import OrderPageApp from "./purchase/OrderPageApp";
 import MyCopang from "./member/MyCopang";
+import TopBar from "./TopBar/TopBar";
+import LoginPage from './TopBar/Component/LoginPage';
+import RegisterPage from './TopBar/Component/RegisterPage';
 
 const RouteMain = () => {
     return (
         <div>
             <BrowserRouter>
-                <Menu />
+                <TopBar />
                 <Route path="/member/1" component={Cart} />
                 <Route path="/member/2" component={OrderPageApp} />
-                {/* <Route path="/member/3" component={sds}/> */}
+                {/* <Route path="/member/3" component={}/> */}
                 <Route path="/member/4" component={ProductListApp}/>
                 <Route path="/member/5" component={MyCopang} />
                 <Route path="/member/6" component={RouteHyunjin} />
